4926a7d4abc2ca243e678ac118431a07bece2303
Kai <9575844+KaiReidCasey@users.noreply.github.com>
2026-02-22 16:39:08 -0500

    Added basic footer and bare bones landing page content

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-
-
 import TopBar from './TopBar';
+import LandingMainBody from './LandingMainBody';
+import Footer from './Footer';
 
 function App() {
-  return <div><TopBar /></div>
+  return <div><TopBar /><LandingMainBody /><Footer /></div>
 }
 
 export default App;
---
+++ src/Footer.tsx
@@ -0,0 +1,8 @@
+
+/* Footer for overall website
+ */
+function Footer() {
+  return <div id="footer">© 2026 by Friends of Maeve J. Dowd</div>;
+}
+
+export default Footer;
