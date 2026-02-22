86f2af86e418e44fd680b3105bf75b22c86e9dc2
Kai <9575844+KaiReidCasey@users.noreply.github.com>
2026-02-22 15:49:18 -0500

    Added start of top bar

---
+++ src/TopBar.tsx
@@ -0,0 +1,12 @@
+
+/* Top bar for overall website
+  TODO:
+    - Add icon
+    - Make clickable
+    - Break down into more parts or at least more readable parts if makes sense as learn more about React
+ */
+function TopBar() {
+  return <div id="topBar"><div>Icon</div><div>Meet Maeve</div><div>Volunteer</div><div>Endorsments</div><div>Community Events</div></div>;
+}
+
+export default TopBar;
