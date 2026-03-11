6b1a8e08133df0d873047bea888a64bf3a01170e
Kai <9575844+KaiReidCasey@users.noreply.github.com>
2026-03-11 09:12:21 -0400

    Added campaign text to for Meet Maeve section (bio & issues)

--- src/MeetMaeve.tsx
+++ src/MeetMaeve.tsx
@@ -1,7 +1,6 @@
 
-/* Top bar for overall website
+/* Meet Maeve
   TODO:
-    - Get actual text
     - Possibly break Issues into own component/s, possibly with pulling text from JSON
  */
 function MeetMaeve() {
@@ -9,28 +8,28 @@ function MeetMaeve() {
     <h2>Meet Maeve</h2>
     <div>
       <h3>Bio</h3>
-      <div>Lorum Ipsum longer bio</div>
+      <div>Maeve is a 42 year old Non-Binary Transfemme who has lived and worked in RI most of their life. They currently reside in Providence with their spouse. Currently a commuter to an out of state office, they have also worked in public facing retail and security jobs throughout the state as well as being a community organizer in both Providence and Newport involved with feeding people through Food Not Bombs in the early 2000’s. They were raised by their father Joseph who fought for custody of both the candidate and their brother post divorce and eventually the three of them moved in with his parents, the candidate’s grandparents Edward and Rita Dowd until moving to Richmond in the late 90’s. They were raised in a working class background, with many of their uncles and older cousins working in fields such as construction and healthcare while their father worked in printing and bindery and later IT. As an adult they have had many experiences including a period of homelessness that has helped them further understand the difficulties and hardships that people face daily.  The candidate earned an associates degree in business during this period and Pell Grants were one of the reasons they were able to find stability with their partner.</div>
     </div>
     <div>
       <h3>Issues</h3>
       <div>
         <h4>Housing</h4>
-        <div>Housing lorum ipsum</div>
+        <div>There is no reason why there is not housing for all Rhode Island residents. Study after study has proven that housing first solutions are best and reduce instances of police action as well as ER usage and improving public access for all.</div>
       </div>
       <div>
         <h4>Healthcare</h4>
-        <div>Healthcare lorum Ipsum</div>
+        <div>Access is hurt by our for profit systems for healthcare. We have had multiple hospitals go on strike recently but the investment to ensure that we have enough professionals in the field to make up the shortfall in care access is not coming. Access to gender affirming care should not be restricted or repealed.</div>
         <div>
-          <h4>Trans Rights</h4>
-          <div>Trans Rights lorum ipsum</div>
+          <h4>Queer Rights</h4>
+          <div>We have nationally and internationally seen the rights of Gender and Sexual Minorities be both trampled on and ignored with people losing access to medicine as well as identification stripped from them or their access limited. While we live in an area that is mostly safe we still see attempts to erode our rights with regards to youth access to sports and similar efforts.</div>
         </div>
         <div>
           <h4>Infrastructure</h4>
-          <div>Infrastructure lorum ipsum</div>
+          <div>Rhode Island has infrastructure that is failing throughout the state. A solution to this is to invest in public transportation more fully in an effort to remove cars from the road as well as adjust statewide zoning rules to allow more local businesses to create more walkable neighborhoods with food access. We need a more accessible last mile for more of our citizens.</div>
         </div>
         <div>
-          <h4>Political Climate / Division / Voting As Harm Reduction Not Winning Strategy</h4>
-          <div>Political Climate / Division / Voting As Harm Reduction Not Winning Strategy lorum ipsum</div>
+          <h4>Rising Costs</h4>
+          <div>Many people, myself included, experience anxiety over the rising costs of housing and energy and struggle to get by because of it. Corporate landlords have made the upward mobility from renter to owner difficult and the lack of investment into renewable energy along with restrictions on extractive energy sources has made our electric bills some of the highest in the area with no signs of relief. - If you want to separate this into renewable investment, make sure if you do, you use language that phrases renewable energy in agrarian terms, like “harvestable energy”.</div>
         </div>
       </div>
     </div>
