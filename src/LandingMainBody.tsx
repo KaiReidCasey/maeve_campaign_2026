d15c68f4aaf325ee2a6c25138503defdcb4179c5
Kai <9575844+KaiReidCasey@users.noreply.github.com>
2026-03-11 10:15:00 -0400

    Cleaned up whitespace Landing Main Body

--- src/LandingMainBody.tsx
+++ src/LandingMainBody.tsx
@@ -2,7 +2,14 @@
 /* Langing page's main body content
  */
 function LandingMainBody() {
-  return <div id="landingMainBody"><div>Meet the Candidate</div><div>Headshot</div><div>Maeve J. Dowd</div><div>Pronouns</div><div>Short Bio</div><div>Prominent link to Donation</div></div>;
+  return <div id="landingMainBody">
+    <div>Meet the Candidate</div>
+    <div>Headshot</div>
+    <div>Maeve J. Dowd</div>
+    <div>Pronouns</div>
+    <div>Short Bio</div>
+    <div>Prominent link to Donation</div>
+  </div>;
 }
 
 export default LandingMainBody;
