551359168b9ab912855e6185b838feee81a38b60
Kai <9575844+KaiReidCasey@users.noreply.github.com>
2026-03-11 10:09:27 -0400

    Made top/nav bar more accessible

--- src/TopBar.tsx
+++ src/TopBar.tsx
@@ -6,13 +6,13 @@
  */
 function TopBar() {
   return <div id="topBar">
-    <div>
+    <nav aria-label="Maeve Dowd Campaign Navigation">
       <button>Icon</button>
       <button>Meet Maeve</button>
       <button>Volunteer</button>
       <button>Endorsements</button>
       <button>Community Events</button>
-    </div>
+    </nav>
     <span id="topBarSpace"></span>
     <div id="topBarQuote">Since you've made my existence political, I've become a politician.</div>
   </div>;
